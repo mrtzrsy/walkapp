628e0ab5fe625e214a49caf2e66c30951c102f6a
mrtzrsy <mrtzrsy@gmail.com>
2025-11-03 02:36:43 +0300

    Refactor main function and step count handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
 
-void main() {
-  runApp(const WalkApp());
-}
+void main() => runApp(const WalkApp());
 
 class WalkApp extends StatefulWidget {
-  const WalkApp({Key? key}) : super(key: key);
-
+  const WalkApp({super.key});
   @override
   State<WalkApp> createState() => _WalkAppState();
 }
 
 class _WalkAppState extends State<WalkApp> {
-  Stream<StepCount>? stepStream;
-  int steps = 0;
+  String steps = "0";
 
   @override
   void initState() {
     super.initState();
-    stepStream = Pedometer.stepCountStream;
-    stepStream!.listen(onStep).onError((e) {
-      print("Hata: $e");
-    });
-  }
-
-  void onStep(StepCount event) {
-    setState(() {
-      steps = event.steps;
+    Pedometer.stepCountStream.listen((StepCount event) {
+      setState(() => steps = event.steps.toString());
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("WalkApp - Adım Sayar")),
+        backgroundColor: Colors.black,
         body: Center(
           child: Text(
-            "$steps Adım",
-            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            steps,
+            style: const TextStyle(color: Colors.white, fontSize: 40),
           ),
         ),
       ),
