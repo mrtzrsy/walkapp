6b67dc45bb352fa719ff1445434b00574a2aef3e
mrtzrsy <mrtzrsy@gmail.com>
2025-11-03 03:03:56 +0300

    Refactor WalkApp and integrate permission handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,58 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(const WalkApp());
-
-class WalkApp extends StatefulWidget {
-  const WalkApp({super.key});
-  @override
-  State<WalkApp> createState() => _WalkAppState();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.activityRecognition.request();
+  runApp(const WalkApp());
 }
 
-class _WalkAppState extends State<WalkApp> {
-  String steps = "0";
+class WalkApp extends StatelessWidget {
+  const WalkApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const StepScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class StepScreen extends StatefulWidget {
+  const StepScreen({super.key});
+
+  @override
+  State<StepScreen> createState() => _StepScreenState();
+}
+
+class _StepScreenState extends State<StepScreen> {
+  StreamSubscription<StepCount>? _subscription;
+  int totalSteps = 0;
 
   @override
   void initState() {
     super.initState();
-    Pedometer.stepCountStream.listen((StepCount event) {
-      setState(() => steps = event.steps.toString());
+    _subscription = Pedometer.stepCountStream.listen((event) {
+      setState(() => totalSteps = event.steps);
     });
   }
 
   @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            steps,
-            style: const TextStyle(color: Colors.white, fontSize: 40),
-          ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("WalkApp - Adım Sayar")),
+      body: Center(
+        child: Text(
+          "$totalSteps Adım",
+          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
         ),
       ),
     );
